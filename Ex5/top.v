64368421ea364470c99062b0077cd8c69836d196
Cloud User <centos@ip-172-31-40-115.eu-west-2.compute.internal>
2021-05-26 12:11:30 +0000

    A complete solution to Ex5

--- Ex5/top.v
+++ Ex5/top.v
@@ -12,4 +12,29 @@
 //  outputs:
 //           heating, cooling
 //////////////////////////////////////////////////////////////////////////////////
+module AC (
+    //ports
+    input clk,
+    input [4:0] temp,
+    output reg h,
+    output reg c 
+);
+// wire
+    wire out;
 
+//logic
+always @(posedge clk)
+    	begin
+	if (temp<=18)
+		c=0;
+		h=1;
+	if (temp>=22)
+		h=0;
+		c=1;
+	if (h==1 && temp>=20)
+		h=0;
+	if (c==1 && temp<=20)
+		c=0;
+	end
+
+endmodule
